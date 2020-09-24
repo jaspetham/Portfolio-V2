0416c30f1703cf7db46efb955a1712ff09273548
jaspertham <jaspertham98@gmail.com>
2020-09-24 17:02:40 +0800

    update works

--- src/app/works/works.component.ts
+++ src/app/works/works.component.ts
@@ -1,22 +1,26 @@
-
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
 
 @Component({
   selector: 'app-works',
   templateUrl: './works.component.html',
-  styleUrls: ['./works.component.css']
+  styleUrls: ['./works.component.css'],
 })
 export class WorksComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    $(function () {
-
-      $(' #da-thumbs > li ').hoverdir();
-
-  });
+    if (
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
+    ) {
+      // some code..
+      console.log('true');
+    } else {
+      $(function () {
+        $(' #da-thumbs > li ').hoverdir();
+      });
+    }
   }
-
 }
